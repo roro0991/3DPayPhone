667619ca18242e7502d3fe7a36fa58891f7707e2
roro0991 <rtchjen@gmail.com>
2024-07-24 18:57:38 -0400

    refactoring and organization
    Created new Dictionary singleton for storing character int values and character segment arrays.

--- 1stPersonTest/Assets/Scripts/PhoneManager.cs
+++ 1stPersonTest/Assets/Scripts/PhoneManager.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 using UnityEngine.VFX;
 
 public class PhoneManager : MonoBehaviour
-{
+{    
     [SerializeField] private PhoneDisplayController phoneDisplayController;
     [SerializeField] private DialogueManager dialogueManager;
         
     public Animator receiverAnimator;
     public Animator cameraAnimator;
         
-    private int?[] phoneNumber = new int?[7]; // to store the player input
+    private int[] phoneNumber = new int[7]; // to store the player input
     private int currentNumberIndex = 0; // to track where in the phone number we are
     private int currentDisplayCharIndex = 28; // to set where to display numbers on phone display
     private int numberAsInt; // to send to call trigger
@@ -26,20 +26,20 @@ public class PhoneManager : MonoBehaviour
         string phoneNumberAsString = string.Join(string.Empty, phoneNumber);
         int.TryParse(phoneNumberAsString, out numberAsInt);
     }
-    public void NumberButton(int number)
+    public void NumberButton(int input)
     {
         if (receiverIsPickedUp)
         {
             if (currentNumberIndex < phoneNumber.Length)
             {
-                phoneNumber[currentNumberIndex] = number;
+                phoneNumber[currentNumberIndex] = input;
                 currentNumberIndex++;
                 if (currentDisplayCharIndex == 31)
                 {
                     phoneDisplayController.chars[currentDisplayCharIndex].GetComponent<CharController>().DisplayDash();
                     currentDisplayCharIndex++;
                 }
-                phoneDisplayController.chars[currentDisplayCharIndex].GetComponent<CharController>().DisplayChar(number);
+                phoneDisplayController.chars[currentDisplayCharIndex].GetComponent<CharController>().DisplayChar(input);
                 currentDisplayCharIndex++;
             }        
         }
--- 1stPersonTest/Assets/Scripts/PuzzleManager.cs
+++ 1stPersonTest/Assets/Scripts/PuzzleManager.cs
@@ -18,50 +18,9 @@ public class PuzzleManager : MonoBehaviour
 
     [SerializeField] private GameObject[] inputChars = new GameObject[7]; // relevent phone display objects for displaying input
     private char[] answerSequence = new char[7]; // correct solution
-    int[] answerSequenceAsInt = new int[7];
+    int[] answerSequenceAsInt = new int[7]; // converted to int to compare to inputsequence
     private int[] inputSequence = new int[7]; // storing input sequence
-    private int currentInputIndex; // tracking where int the input sequence we are    
-
-    // jagged array for storing relevant display char segments for each possible number
-    int[][] numbersArray = new int[][]
-        { new int[] { 0, 1, 2, 3, 4, 5, 10, 11 }, //0
-          new int[] { 1, 2, 10 }, //1
-          new int[] { 0, 1, 3, 4, 6, 7 }, //2
-          new int[] { 0, 1, 2, 3, 7 }, //3
-          new int[] { 1, 2, 5, 6, 7 }, // 4
-          new int[] { 0, 2, 3, 5, 6, 7 }, // 5
-          new int[] { 0, 2, 3, 4, 5, 6, 7 }, //6
-          new int[] { 0, 1, 2 }, //7
-          new int[] { 0, 1, 2, 3, 4, 5, 6, 7}, //8
-          new int[] { 0, 1, 2, 3, 5, 6, 7 }, //9
-          new int[] { 0, 1, 2, 4, 5, 6, 7}, //a
-          new int[] { 0, 1, 2, 3, 7, 9, 12}, //b
-          new int[] { 0, 3, 4, 5}, //c
-          new int[] { 0, 1, 2, 3, 9, 12}, //d
-          new int[] { 0, 3, 4, 5, 6}, //e
-          new int[] { 0, 4, 5, 6}, //f
-          new int[] { 0, 2, 3, 4, 5, 7}, //g
-          new int[] { 1, 2, 4, 5, 6, 7}, //h
-          new int[] { 0, 3, 9, 12}, //i
-          new int[] { 1, 2, 3, 4}, //j
-          new int[] { 4, 5, 6, 10, 13}, //k
-          new int[] { 3, 4, 5}, //l
-          new int[] { 1, 2, 4, 5, 8, 10}, //m
-          new int[] { 1, 2, 4, 5, 8, 13}, //n
-          new int[] { 0, 1, 2, 3, 4, 5,}, //o
-          new int[] { 0, 1, 4, 5, 6, 7}, //p
-          new int[] { 0, 1, 2, 3, 4, 5, 13}, //q
-          new int[] { 0, 1, 4, 5, 6, 7, 13}, //r
-          new int[] { 0, 2, 3, 5, 6, 7}, //s
-          new int[] { 0, 9, 12}, //t
-          new int[] { 1, 2, 3, 4}, //u
-          new int[] { 4, 5, 10, 11}, //v
-          new int[] { 1, 2, 4, 5, 11, 13}, //w
-          new int[] { 8, 10, 11, 13}, //x
-          new int[] { 8, 10, 12}, //y
-          new int[] { 0, 3, 10, 11} //z
-
-        };
+    private int currentInputIndex; // tracking where int the input sequence we are       
 
     // relevent display objects for randomizing number segments
     [SerializeField] private GameObject[] puzzleColumnOne = new GameObject[4];
@@ -74,50 +33,11 @@ public class PuzzleManager : MonoBehaviour
 
     private List<GameObject[]> puzzleColumns = new List<GameObject[]>(); // list of display column arrays above
 
-    bool inNumbers = true;
+    private bool inNumbers = true;
     private bool isPuzzleSolved;
 
     private void Start()
     {
-        Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
-        keyValuePairs.Add('0', 0);
-        keyValuePairs.Add('1', 1);
-        keyValuePairs.Add('2', 2);
-        keyValuePairs.Add('3', 3);
-        keyValuePairs.Add('4', 4);
-        keyValuePairs.Add('5', 5);
-        keyValuePairs.Add('6', 6);
-        keyValuePairs.Add('7', 7);
-        keyValuePairs.Add('8', 8);
-        keyValuePairs.Add('9', 9);
-        keyValuePairs.Add('a', 10);
-        keyValuePairs.Add('b', 11);
-        keyValuePairs.Add('c', 12);
-        keyValuePairs.Add('d', 13);
-        keyValuePairs.Add('e', 14);
-        keyValuePairs.Add('f', 15);
-        keyValuePairs.Add('g', 16);
-        keyValuePairs.Add('h', 17);
-        keyValuePairs.Add('i', 18);
-        keyValuePairs.Add('j', 19);
-        keyValuePairs.Add('k', 20);
-        keyValuePairs.Add('l', 21);
-        keyValuePairs.Add('m', 22);
-        keyValuePairs.Add('n', 23);
-        keyValuePairs.Add('o', 24);
-        keyValuePairs.Add('p', 25);
-        keyValuePairs.Add('q', 26);
-        keyValuePairs.Add('r', 27);
-        keyValuePairs.Add('s', 28);
-        keyValuePairs.Add('t', 29);
-        keyValuePairs.Add('u', 30);
-        keyValuePairs.Add('v', 31);
-        keyValuePairs.Add('w', 32);
-        keyValuePairs.Add('x', 33);
-        keyValuePairs.Add('y', 34);
-        keyValuePairs.Add('z', 35);
-
-
         answerSequence = puzzle.answerSequence;
 
         puzzleColumns.AddRange(new List<GameObject[]>()
@@ -134,7 +54,7 @@ public class PuzzleManager : MonoBehaviour
         int index = 0;
         foreach (char character in answerSequence)
         {
-            int characterAsInt = keyValuePairs[character];
+            int characterAsInt = Dictionary.GetInstance().charIntPairs[character];
             answerSequenceAsInt[index] = characterAsInt;
             FillPuzzleColumn(puzzleColumns[index], characterAsInt);
             index++;
@@ -180,13 +100,13 @@ public class PuzzleManager : MonoBehaviour
 
         int randomizedColumnSegmentsIndex = 0;
         int numberOfColumnIterations = 0;
-        for (int i = 0; i < numbersArray[correctNumber].Length; i++)
+        for (int i = 0; i < Dictionary.GetInstance().charSegments[correctNumber].Length; i++)
         {
             if (numberOfColumnIterations < column.Length)
             {
                 column[randomizedColumnSegments[randomizedColumnSegmentsIndex]].
                 GetComponent<CharController>().
-                DisplaySegment(numbersArray[correctNumber][i]);
+                DisplaySegment(Dictionary.GetInstance().charSegments[correctNumber][i]);
                 numberOfColumnIterations++;
                 randomizedColumnSegmentsIndex++; 
             }
@@ -196,7 +116,7 @@ public class PuzzleManager : MonoBehaviour
                 randomizedColumnSegmentsIndex = 0;
                 column[randomizedColumnSegments[randomizedColumnSegmentsIndex]].
                 GetComponent<CharController>().
-                DisplaySegment(numbersArray[correctNumber][i]);                
+                DisplaySegment(Dictionary.GetInstance().charSegments[correctNumber][i]);                
                 randomizedColumnSegmentsIndex++;
             }
         }
